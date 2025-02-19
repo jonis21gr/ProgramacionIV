39a73ade4a494a0183bf11647ec8c76d791cbc63
jonis21gr <reynaga.jonis2004@gmail.com>
2025-02-19 11:22:48 -0600

    Add files via upload

---
+++ practica5 - copia/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace practica5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Label();
+            this.lbllastname = new System.Windows.Forms.Label();
+            this.CONTRY = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtlast = new System.Windows.Forms.TextBox();
+            this.cbboxPais = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnr = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(34, 53);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(112, 37);
+            this.name.TabIndex = 0;
+            this.name.Text = "NAME";
+            this.name.Click += new System.EventHandler(this.name_Click);
+            // 
+            // lbllastname
+            // 
+            this.lbllastname.AutoSize = true;
+            this.lbllastname.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllastname.Location = new System.Drawing.Point(34, 163);
+            this.lbllastname.Name = "lbllastname";
+            this.lbllastname.Size = new System.Drawing.Size(203, 37);
+            this.lbllastname.TabIndex = 1;
+            this.lbllastname.Text = "LAST NAME";
+            // 
+            // CONTRY
+            // 
+            this.CONTRY.AutoSize = true;
+            this.CONTRY.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CONTRY.Location = new System.Drawing.Point(441, 53);
+            this.CONTRY.Name = "CONTRY";
+            this.CONTRY.Size = new System.Drawing.Size(153, 37);
+            this.CONTRY.TabIndex = 2;
+            this.CONTRY.Text = "CONTRY";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(162, 53);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(273, 45);
+            this.txtname.TabIndex = 3;
+            // 
+            // txtlast
+            // 
+            this.txtlast.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlast.Location = new System.Drawing.Point(243, 163);
+            this.txtlast.Name = "txtlast";
+            this.txtlast.Size = new System.Drawing.Size(351, 45);
+            this.txtlast.TabIndex = 4;
+            // 
+            // cbboxPais
+            // 
+            this.cbboxPais.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbboxPais.FormattingEnabled = true;
+            this.cbboxPais.Location = new System.Drawing.Point(600, 50);
+            this.cbboxPais.Name = "cbboxPais";
+            this.cbboxPais.Size = new System.Drawing.Size(308, 45);
+            this.cbboxPais.TabIndex = 5;
+            this.cbboxPais.SelectedIndexChanged += new System.EventHandler(this.cbboxPais_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(214, 326);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(516, 204);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "NAME";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "LAST NAME";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "CONTRY";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.Location = new System.Drawing.Point(231, 236);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(101, 56);
+            this.btnadd.TabIndex = 7;
+            this.btnadd.Text = "ADD";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnr
+            // 
+            this.btnr.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnr.Location = new System.Drawing.Point(529, 236);
+            this.btnr.Name = "btnr";
+            this.btnr.Size = new System.Drawing.Size(169, 56);
+            this.btnr.TabIndex = 8;
+            this.btnr.Text = "REMOVE";
+            this.btnr.UseVisualStyleBackColor = true;
+            this.btnr.Click += new System.EventHandler(this.btnr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 542);
+            this.Controls.Add(this.btnr);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbboxPais);
+            this.Controls.Add(this.txtlast);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.CONTRY);
+            this.Controls.Add(this.lbllastname);
+            this.Controls.Add(this.name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label lbllastname;
+        private System.Windows.Forms.Label CONTRY;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtlast;
+        private System.Windows.Forms.ComboBox cbboxPais;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnr;
+    }
+}
+
---
+++ practica5 - copia/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica5
+{
+    public partial class Form1 : Form
+    {
+        //agregamos una variable que ocuparemos en otras lineas
+        int vIndice;// no guarda el indice seleccionado en la table
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lineas de codigo para agregar los paises 
+            cbboxPais.Items.Add("Mexico");
+            cbboxPais.Items.Add("Alemania");
+            cbboxPais.Items.Add("China");
+            cbboxPais.Items.Add("Japon");
+            cbboxPais.Items.Add("España");
+        }
+        String Vseleccion; //se agrega una variable en tipo texto para hacer cadena de texto
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //linea de cofigo para agregar cada dao a cada campo que correspode
+            Vseleccion = Convert.ToString(cbboxPais.Text);
+            int vIndice = dataGridView1.Rows.Add();
+            dataGridView1.Rows[vIndice].Cells[0].Value = txtname.Text;
+            dataGridView1.Rows[vIndice].Cells[1].Value = txtlast.Text;
+            dataGridView1.Rows[vIndice].Cells[2].Value = Vseleccion;
+
+            //codigo para poner en blanco los text
+            txtname.Text = "";
+            txtlast.Text = "";
+            cbboxPais.Text = "";
+
+        }
+
+        private void cbboxPais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            vIndice = e.RowIndex;
+        }
+
+        private void btnr_Click(object sender, EventArgs e)
+        {// linea para que se elemine los daos seliccionados en la tabla los cuales se guardaron en la variable vindece
+            if (vIndice != -1)
+            {
+                dataGridView1.Rows.RemoveAt(vIndice);
+            }
+            else
+            {
+                MessageBox.Show("selected Item Incorrect");
+            }
+        }
+       
+
+
+    }
+}
