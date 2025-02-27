300bd498467348171ff44c9122ca57a1612c5925
jonis21gr <reynaga.jonis2004@gmail.com>
2025-02-26 19:38:46 -0600

    Add files via upload

---
+++ examen/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿using System;
+
+namespace examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Enero = new System.Windows.Forms.RadioButton();
+            this.Febrero = new System.Windows.Forms.RadioButton();
+            this.Marzo = new System.Windows.Forms.RadioButton();
+            this.Abril = new System.Windows.Forms.RadioButton();
+            this.Mayo = new System.Windows.Forms.RadioButton();
+            this.Julio = new System.Windows.Forms.RadioButton();
+            this.Agosto = new System.Windows.Forms.RadioButton();
+            this.Septiembre = new System.Windows.Forms.RadioButton();
+            this.Octubre = new System.Windows.Forms.RadioButton();
+            this.Noviembre = new System.Windows.Forms.RadioButton();
+            this.Diciembre = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Junio = new System.Windows.Forms.RadioButton();
+            this.btnsigno = new System.Windows.Forms.Button();
+            this.txtsigno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Enero
+            // 
+            this.Enero.AutoSize = true;
+            this.Enero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Enero.Location = new System.Drawing.Point(13, 147);
+            this.Enero.Name = "Enero";
+            this.Enero.Size = new System.Drawing.Size(77, 24);
+            this.Enero.TabIndex = 0;
+            this.Enero.TabStop = true;
+            this.Enero.Text = "Enero";
+            this.Enero.UseVisualStyleBackColor = false;
+            // 
+            // Febrero
+            // 
+            this.Febrero.AutoSize = true;
+            this.Febrero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Febrero.Location = new System.Drawing.Point(13, 177);
+            this.Febrero.Name = "Febrero";
+            this.Febrero.Size = new System.Drawing.Size(90, 24);
+            this.Febrero.TabIndex = 1;
+            this.Febrero.TabStop = true;
+            this.Febrero.Text = "Febrero";
+            this.Febrero.UseVisualStyleBackColor = false;
+            // 
+            // Marzo
+            // 
+            this.Marzo.AutoSize = true;
+            this.Marzo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Marzo.Location = new System.Drawing.Point(13, 207);
+            this.Marzo.Name = "Marzo";
+            this.Marzo.Size = new System.Drawing.Size(78, 24);
+            this.Marzo.TabIndex = 2;
+            this.Marzo.TabStop = true;
+            this.Marzo.Text = "Marzo";
+            this.Marzo.UseVisualStyleBackColor = false;
+            // 
+            // Abril
+            // 
+            this.Abril.AutoSize = true;
+            this.Abril.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Abril.Location = new System.Drawing.Point(13, 237);
+            this.Abril.Name = "Abril";
+            this.Abril.Size = new System.Drawing.Size(65, 24);
+            this.Abril.TabIndex = 3;
+            this.Abril.TabStop = true;
+            this.Abril.Text = "Abril";
+            this.Abril.UseVisualStyleBackColor = false;
+            // 
+            // Mayo
+            // 
+            this.Mayo.AutoSize = true;
+            this.Mayo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Mayo.Location = new System.Drawing.Point(13, 267);
+            this.Mayo.Name = "Mayo";
+            this.Mayo.Size = new System.Drawing.Size(72, 24);
+            this.Mayo.TabIndex = 4;
+            this.Mayo.TabStop = true;
+            this.Mayo.Text = "Mayo";
+            this.Mayo.UseVisualStyleBackColor = false;
+            this.Mayo.CheckedChanged += new System.EventHandler(this.Mayo_CheckedChanged);
+            // 
+            // Julio
+            // 
+            this.Julio.AutoSize = true;
+            this.Julio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Julio.Location = new System.Drawing.Point(164, 147);
+            this.Julio.Name = "Julio";
+            this.Julio.Size = new System.Drawing.Size(66, 24);
+            this.Julio.TabIndex = 6;
+            this.Julio.TabStop = true;
+            this.Julio.Text = "Julio";
+            this.Julio.UseVisualStyleBackColor = false;
+            // 
+            // Agosto
+            // 
+            this.Agosto.AutoSize = true;
+            this.Agosto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Agosto.Location = new System.Drawing.Point(164, 177);
+            this.Agosto.Name = "Agosto";
+            this.Agosto.Size = new System.Drawing.Size(85, 24);
+            this.Agosto.TabIndex = 7;
+            this.Agosto.TabStop = true;
+            this.Agosto.Text = "Agosto";
+            this.Agosto.UseVisualStyleBackColor = false;
+            // 
+            // Septiembre
+            // 
+            this.Septiembre.AutoSize = true;
+            this.Septiembre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Septiembre.Location = new System.Drawing.Point(164, 207);
+            this.Septiembre.Name = "Septiembre";
+            this.Septiembre.Size = new System.Drawing.Size(116, 24);
+            this.Septiembre.TabIndex = 8;
+            this.Septiembre.TabStop = true;
+            this.Septiembre.Text = "Septiembre";
+            this.Septiembre.UseVisualStyleBackColor = false;
+            // 
+            // Octubre
+            // 
+            this.Octubre.AutoSize = true;
+            this.Octubre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Octubre.Location = new System.Drawing.Point(164, 237);
+            this.Octubre.Name = "Octubre";
+            this.Octubre.Size = new System.Drawing.Size(91, 24);
+            this.Octubre.TabIndex = 9;
+            this.Octubre.TabStop = true;
+            this.Octubre.Text = "Octubre";
+            this.Octubre.UseVisualStyleBackColor = false;
+            // 
+            // Noviembre
+            // 
+            this.Noviembre.AutoSize = true;
+            this.Noviembre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Noviembre.Location = new System.Drawing.Point(164, 267);
+            this.Noviembre.Name = "Noviembre";
+            this.Noviembre.Size = new System.Drawing.Size(109, 24);
+            this.Noviembre.TabIndex = 10;
+            this.Noviembre.TabStop = true;
+            this.Noviembre.Text = "Noviembre";
+            this.Noviembre.UseVisualStyleBackColor = false;
+            // 
+            // Diciembre
+            // 
+            this.Diciembre.AutoSize = true;
+            this.Diciembre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Diciembre.Location = new System.Drawing.Point(164, 297);
+            this.Diciembre.Name = "Diciembre";
+            this.Diciembre.Size = new System.Drawing.Size(105, 24);
+            this.Diciembre.TabIndex = 11;
+            this.Diciembre.TabStop = true;
+            this.Diciembre.Text = "Diciembre";
+            this.Diciembre.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.comboBox1.Location = new System.Drawing.Point(499, 136);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 41);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "DIA";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Junio
+            // 
+            this.Junio.AutoSize = true;
+            this.Junio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Junio.Location = new System.Drawing.Point(12, 297);
+            this.Junio.Name = "Junio";
+            this.Junio.Size = new System.Drawing.Size(72, 24);
+            this.Junio.TabIndex = 13;
+            this.Junio.TabStop = true;
+            this.Junio.Text = "Junio";
+            this.Junio.UseVisualStyleBackColor = false;
+            // 
+            // btnsigno
+            // 
+            this.btnsigno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnsigno.Location = new System.Drawing.Point(94, 354);
+            this.btnsigno.Name = "btnsigno";
+            this.btnsigno.Size = new System.Drawing.Size(158, 84);
+            this.btnsigno.TabIndex = 14;
+            this.btnsigno.Text = "OBTENER SIGNO ZODIACAL";
+            this.btnsigno.UseVisualStyleBackColor = false;
+            this.btnsigno.Click += new System.EventHandler(this.btnsigno_Click);
+            // 
+            // txtsigno
+            // 
+            this.txtsigno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtsigno.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsigno.Location = new System.Drawing.Point(447, 411);
+            this.txtsigno.Name = "txtsigno";
+            this.txtsigno.Size = new System.Drawing.Size(179, 40);
+            this.txtsigno.TabIndex = 15;
+            this.txtsigno.Text = "Signo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(210, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 37);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "SIGNO ZODIACAL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(670, 495);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsigno);
+            this.Controls.Add(this.btnsigno);
+            this.Controls.Add(this.Junio);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Diciembre);
+            this.Controls.Add(this.Noviembre);
+            this.Controls.Add(this.Octubre);
+            this.Controls.Add(this.Septiembre);
+            this.Controls.Add(this.Agosto);
+            this.Controls.Add(this.Julio);
+            this.Controls.Add(this.Mayo);
+            this.Controls.Add(this.Abril);
+            this.Controls.Add(this.Marzo);
+            this.Controls.Add(this.Febrero);
+            this.Controls.Add(this.Enero);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Mayo_CheckedChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton Enero;
+        private System.Windows.Forms.RadioButton Febrero;
+        private System.Windows.Forms.RadioButton Marzo;
+        private System.Windows.Forms.RadioButton Abril;
+        private System.Windows.Forms.RadioButton Mayo;
+        private System.Windows.Forms.RadioButton Julio;
+        private System.Windows.Forms.RadioButton Agosto;
+        private System.Windows.Forms.RadioButton Septiembre;
+        private System.Windows.Forms.RadioButton Octubre;
+        private System.Windows.Forms.RadioButton Noviembre;
+        private System.Windows.Forms.RadioButton Diciembre;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton Junio;
+        private System.Windows.Forms.Button btnsigno;
+        private System.Windows.Forms.TextBox txtsigno;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ examen/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //este apartado ejecuta cuando cambia el estado del RadioButton
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        //ejecuta cuando el usuario selecciona un día en el ComboBox
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //ahora si se viene lo bueno
+       //este boton al momento de dar clicl va seleccionar el signo denro de este boton viene todo programado
+        private void btnsigno_Click(object sender, EventArgs e)
+        {
+            //el usuario seleciona en el combobox un dia por eso utilizamos la propiedad selecteditem
+            //el null es para dar entender que no ahi nada seleccionado
+            if (comboBox1.SelectedItem == null)
+            {
+                // Muestra un mensaje de error si no se ha seleccionado un día por eso se utilzo anteriorente el ==null
+                MessageBox.Show("Seleccione un día.");
+            }
+            // Convierte el día seleccionado en un número entero
+            // aqui mismo declaramos el dia con un int y el mes con un string
+            int day = Convert.ToInt32(comboBox1.SelectedItem);
+            // Obtiene el mes seleccionado de los RadioButtons
+            //el getselectedmonth es una funcion que es para obtener el mes seleccionado entre todos los radiobuton
+            //y madar el texto del mes seleccionado por el usuario
+            string month = GetSelectedMonth();
+            // invesigado en google un metodo y encontre el isnullorempty este lo implemente para detectar si el mes esta vacio
+           
+            if (string.IsNullOrEmpty(month))
+            {
+                //Muestra un mensaje de error si no se ha seleccionado un mes
+                MessageBox.Show("Seleccione un mes.");
+            }
+            // Obtiene el signo zodiacal correspondiente con la variable string zodiacal al día y mes seleccionados 
+            // el getzodiacalsing me ayudara para implemetar un forech donde me sa el dia y el mes
+            string zodiacal = GetZodiacSign(day, month);
+            // este txtbox muestra el signo zodiacal al usuario
+            txtsigno.Text = zodiacal;
+        }
+
+        private string GetSelectedMonth()
+        {
+            // Recorre todos los controles dentro del formulario
+            foreach (Control control in this.Controls)
+            {
+                // Verifica si el control es un RadioButton y si está seleccionado Checked == true
+                if (control is RadioButton rb && rb.Checked)
+                {
+                    return rb.Text; // Devuelve el texto del RadioButton seleccionado que representa el mes
+                }
+            }
+            return string.Empty; // Si no se encuentra ningún mes seleccionado, devuelve una cadena vacía
+        }
+
+        private string GetZodiacSign(int day, string month)
+        {
+            switch (month)
+            {
+                case "Enero": return day <= 19 ? "Capricornio" : "Acuario";
+                case "Febrero": return day <= 18 ? "Acuario" : "Piscis";
+                case "Marzo": return day <= 20 ? "Piscis" : "Aries";
+                case "Abril": return day <= 19 ? "Aries" : "Tauro";
+                case "Mayo": return day <= 20 ? "Tauro" : "Géminis";
+                case "Junio": return day <= 20 ? "Géminis" : "Cáncer";
+                case "Julio": return day <= 22 ? "Cáncer" : "Leo";
+                case "Agosto": return day <= 22 ? "Leo" : "Virgo";
+                case "Septiembre": return day <= 22 ? "Virgo" : "Libra";
+                case "Octubre": return day <= 22 ? "Libra" : "Escorpio";
+                case "Noviembre": return day <= 21 ? "Escorpio" : "Sagitario";
+                case "Diciembre": return day <= 21 ? "Sagitario" : "Capricornio";
+                default: return "Desconocido";
+            }
+        }
+    }
+}
